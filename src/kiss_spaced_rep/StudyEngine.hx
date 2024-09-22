87c781199fdd78f9bc20bf3ec1acaf03405e0b28
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-22 09:33:55 -0600

    try add InputType enum

--- src/kiss_spaced_rep/StudyEngine.hx
+++ src/kiss_spaced_rep/StudyEngine.hx
@@ -2,12 +2,17 @@ package kiss_spaced_rep;
 
 typedef Continuation = Void -> Void;
 
+enum InputType {
+    Text;
+    OneToFive;
+}
+
 interface StudyEngine {
     function clear():Void;
     function print(s:String):Void;
     function println(s:String):Void;
     function showImage(s:String):Void;
     function delayForUserInput(cc:Continuation, ?text:String):Void;
-    function getUserInput(resolve:String->Void):Void;
+    function getUserInput(resolve:String->Void, type:InputType):Void;
     function promptForRefresh(refresh:Void->Void):Void;
 }
